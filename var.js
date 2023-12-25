30e3281ce422511525997da068713ea62317e331
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2023-12-25 11:53:59 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,18 @@
+const collegeID =2237710
+let  collegemail = "2237710.cec@cgc.edu.in "
+var accountPassword ="191079509516"
+accountCity ="Landran"
+
+//console.log(collegeID)
+/*
+Don't use var because of issue in block scope and functional scope 
+
+*/
+
+collegemail ="shubhambtps@cgc.edu.in"
+accountPassword ="Simba123" 
+accountCity ="kharar"
+
+
+
+console.table([collegeID,collegemail, accountPassword ,accountCity])
