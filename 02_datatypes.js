dde232165723d87d64b0c12a58a27111cfa598f1
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-01-22 13:28:47 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,5 @@
+   "use  strict"  // It will take all JS code as newer version 
+
+  //  alert (3 + 3) In Node Js it is not applicable as it is not a browser 
+
+       
---
+++ 03_datatypeconversion.js
@@ -0,0 +1,22 @@
+//1st Example
+let score = 33
+
+console.log(typeof score) ;
+console.log(typeof(score));  // Method kyuki parenthesis ke andar 
+
+//Type Conversion 
+
+let valueInString = String(score)
+console.log(typeof valueInString);
+
+// 2nd Example 
+
+let marks = "33"
+
+console.log(typeof marks) ;
+
+//Type Conversion 
+
+let valueInNumber = Number(marks)
+console.log(typeof valueInNumber);
+
