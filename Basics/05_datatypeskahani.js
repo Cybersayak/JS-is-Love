501d6d7fc9b67e51202d9628aa26c37d4a4774d6
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-26 11:57:25 +0000

    Heap Memory

--- Basics/05_datatypeskahani.js
+++ Basics/05_datatypeskahani.js
@@ -3,6 +3,18 @@ let myhomename= "Sayak"
 let asname=myhomename
 asname = "Sakku Bhaiya"
 
+console.log(myhomename);
+console.log(asname);
+// in the above example the primitive values the stored in stack and just give copy of the variable not the exact value 
 
-console.log(asname);
-console.log(asname);
+// Example of Heap Memory in which variable 2 directly refers the value from variable 1 
+let userSayak = {
+    email: "sayak@gmail.com",
+    upi : "sayak@ybl"
+}
+let userTwo = userSayak
+
+userTwo.email = "ghosh@gmail.com"
+
+console.log(userSayak.email);
+console.log(userTwo.email);
