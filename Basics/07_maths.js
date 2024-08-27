3d6d1d082cfb32da9d7431072c52f2da14dc1b70
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-27 07:29:47 +0000

    number in js

---
+++ Basics/07_maths.js
@@ -0,0 +1,12 @@
+const score = 400 // Automatically defined as number 
+console.log(score);
+
+const balance = new Number(500) // Specially declared as number 
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const newscore = 25.899
+console.log(newscore.toPrecision(3));
+
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Repository to learn javascript core concepts for beginners easily by SAYAK GHOSH
 ## check 21daysJavascript
 
 This JavaScript repository is build on the concept of this repository of JS and this 21days of solving JS question to be a entry level javascript expert 
+
+[JavaScript](https://javascript.info/)
