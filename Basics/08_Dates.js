d5b0d16cc07a5e8e5ed8e955d345c0a2cf85c8f7
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-28 18:23:30 +0000

    Date and Time

--- Basics/08_Dates.js
+++ Basics/08_Dates.js
@@ -1,4 +1,14 @@
 // DATES 
 
 let myDate = new Date ();
+console.log(typeof myDate);
+console.log(myDate.toString());
 console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date (2023 , 1 ,14)
+let myCreatedDateTime = new Date (2023 , 7 ,14, 5 ,30, 12)
+
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDateTime.toLocaleString());
