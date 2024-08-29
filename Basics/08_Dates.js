6574e619b5ffbb48fd00077b494613a42261f170
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-29 15:14:05 +0000

    properties of dates in js

--- Basics/08_Dates.js
+++ Basics/08_Dates.js
@@ -1,14 +1,32 @@
 // DATES 
 
-let myDate = new Date ();
-console.log(typeof myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
+// let myDate = new Date ();
+// // console.log(typeof myDate);
+// // console.log(myDate.toString());
+// // console.log(myDate.toDateString());
+// // console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date (2023 , 1 ,14)
+// let myCreatedDateTime = new Date (2023 , 7 ,14, 5 ,30, 12)
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDateTime.toLocaleString());
+
+// let newCreatedDate = new Date ("01-14-2024") 
+// let myTimeStamp = Date.now()
+
+// // console.log(myTimeStamp);
+// // console.log (newCreatedDate.getTime());
+// // console.log (newCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000)); 
 
 
-let myCreatedDate = new Date (2023 , 1 ,14)
-let myCreatedDateTime = new Date (2023 , 7 ,14, 5 ,30, 12)
+let newDate = new Date
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
 
-console.log(myCreatedDate.toDateString());
-console.log(myCreatedDateTime.toLocaleString());
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
