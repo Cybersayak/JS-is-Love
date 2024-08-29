270e105947eea2112eb22574f253ada3f8d2c114
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-08-29 15:15:55 +0000

    All Properties of Dates in JS

--- Basics/08_Dates.js
+++ Basics/08_Dates.js
@@ -1,25 +1,25 @@
 // DATES 
 
-// let myDate = new Date ();
-// // console.log(typeof myDate);
-// // console.log(myDate.toString());
-// // console.log(myDate.toDateString());
-// // console.log(myDate.toLocaleString());
+let myDate = new Date ();
+console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
 
-// let myCreatedDate = new Date (2023 , 1 ,14)
-// let myCreatedDateTime = new Date (2023 , 7 ,14, 5 ,30, 12)
+let myCreatedDate = new Date (2023 , 1 ,14)
+let myCreatedDateTime = new Date (2023 , 7 ,14, 5 ,30, 12)
 
-// console.log(myCreatedDate.toDateString());
-// console.log(myCreatedDateTime.toLocaleString());
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDateTime.toLocaleString());
 
-// let newCreatedDate = new Date ("01-14-2024") 
-// let myTimeStamp = Date.now()
+let newCreatedDate = new Date ("01-14-2024") 
+let myTimeStamp = Date.now()
 
-// // console.log(myTimeStamp);
-// // console.log (newCreatedDate.getTime());
-// // console.log (newCreatedDate.getTime());
+console.log(myTimeStamp);
+console.log (newCreatedDate.getTime());
+console.log (newCreatedDate.getTime());
 
-// console.log(Math.floor(Date.now()/1000)); 
+console.log(Math.floor(Date.now()/1000)); 
 
 
 let newDate = new Date
