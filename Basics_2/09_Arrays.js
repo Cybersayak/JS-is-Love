8305707ade7494f0ff91e8d1b14a2ffd8decb72d
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-02 18:15:27 +0000

    Methods in Arrays

--- Basics_2/09_Arrays.js
+++ Basics_2/09_Arrays.js
@@ -1,4 +1,13 @@
 const myArr = [0 ,1 ,2, 3, 4, 5, 6, 7, 8]
+const myHeroines = ["sunny leone", "nora"]
+const myArr2 = new Array(4,5,8)
 
-console.log(myArr[0]);
 
+console.log(myArr[3]);
+
+// ARRAY METHODS
+
+// myArr.push(6)
+myArr.pop(7)
+//myArr.pop(8)
+console.log(myArr);
