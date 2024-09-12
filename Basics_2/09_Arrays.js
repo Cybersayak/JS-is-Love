5b29a986cb4240baee9086022710a4801e83b5e0
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-12 18:13:26 +0000

    array learning ....

--- Basics_2/09_Arrays.js
+++ Basics_2/09_Arrays.js
@@ -1,13 +1,19 @@
-const myArr = [0 ,1 ,2, 3, 4, 5, 6, 7, 8]
+const myArr = [1 ,2, 9, 4, 5, 6, 7, 8]
 const myHeroines = ["sunny leone", "nora"]
 const myArr2 = new Array(4,5,8)
 
 
-console.log(myArr[3]);
+//console.log(myArr[3]);
 
 // ARRAY METHODS
-
+   
 // myArr.push(6)
-myArr.pop(7)
-//myArr.pop(8)
-console.log(myArr);
+// myArr.pop(7)
+// myArr.pop(8)
+// myArr.unshift(9)
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+
+// console.log(myArr);
+
