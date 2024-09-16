eb4dbe9dcd460b20d850cdd02a7f409cdf7ed91d
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-16 10:51:49 +0000

    Slice and Splice

--- Basics_2/09_Arrays.js
+++ Basics_2/09_Arrays.js
@@ -1,11 +1,11 @@
-const myArr = [1 ,2, 9, 4, 5, 6, 7, 8]
+const myArr = [0 ,1 ,2, 3, 4, 5, 6, 7, 8]
 const myHeroines = ["sunny leone", "nora"]
 const myArr2 = new Array(4,5,8)
 
 
-//console.log(myArr[3]);
+console.log(myArr[3]);
 
-// ARRAY METHODS
+//ARRAY METHODS
    
 // myArr.push(6)
 // myArr.pop(7)
@@ -15,5 +15,26 @@ const myArr2 = new Array(4,5,8)
 console.log(myArr.includes(9));
 console.log(myArr.indexOf(9));
 
-// console.log(myArr);
+console.log(myArr);
 
+const newArray= myArr.join() ///converts the array  to string 
+
+
+console.log(myArr);
+console.log(newArray);
+console.log(typeof newArray);
+
+
+// SLICE 
+console.log("A" ,myArr);
+
+const myn1= myArr.slice(0,3)
+// printing the array with slice ops
+console.log(myn1);
+console.log("B" ,myArr);
+
+// SPLICE
+const myn2= myArr.splice(1,3)
+
+console.log("C" ,myArr);
+console.log(myn2);
