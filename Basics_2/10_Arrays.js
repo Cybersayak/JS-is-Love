9aa01cebe08b8392a78bca0f252603bb22ddb8a4
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-18 17:38:06 +0000

    Push , Concatenation , Spread

---
+++ Basics_2/10_Arrays.js
@@ -0,0 +1,12 @@
+const superheros = ["Chaddiman" , "Shaktimman" ,"Hanuman"]
+const hero =["SuperMan" , "Batman", "SpiderMan"]
+
+// hero.push(superheros)
+// console.log(hero);
+// OUTPUT ['SuperMan','Batman','SpiderMan', [ 'Chaddiman', 'Shaktimman', 'Hanuman' ]]
+
+// let allheros= superheros.concat(hero)
+// console.log(allheros);
+
+// Spread Operator 
+const newheros=[...superheros, ...hero]
