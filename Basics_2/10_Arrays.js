ae89cac5632713276c6cd4a02658b326801aaf0b
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-19 02:46:43 +0000

    continues...

--- Basics_2/10_Arrays.js
+++ Basics_2/10_Arrays.js
@@ -1,12 +1,13 @@
 const superheros = ["Chaddiman" , "Shaktimman" ,"Hanuman"]
 const hero =["SuperMan" , "Batman", "SpiderMan"]
 
-// hero.push(superheros)
-// console.log(hero);
-// OUTPUT ['SuperMan','Batman','SpiderMan', [ 'Chaddiman', 'Shaktimman', 'Hanuman' ]]
+hero.push(superheros)
+console.log(hero);
+//OUTPUT ['SuperMan','Batman','SpiderMan', [ 'Chaddiman', 'Shaktimman', 'Hanuman' ]]
 
-// let allheros= superheros.concat(hero)
-// console.log(allheros);
+let allheros= superheros.concat(hero)
+console.log(allheros);
 
 // Spread Operator 
 const newheros=[...superheros, ...hero]
+console.log(newheros);
