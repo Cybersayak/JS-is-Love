ccfc3f95d7d0ab8b4badca84c2b5a61ba0e2b7ed
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-09-25 08:17:17 +0000

    Arrays Completed

--- Basics_2/10_Arrays.js
+++ Basics_2/10_Arrays.js
@@ -18,3 +18,16 @@ const newArray= [1,2,3,[4,5,6],7,[8,9[10,11]]];
 
 const usableArray= newArray.flat(Infinity);
 console.log(usableArray);
+
+
+console.log(Array.isArray("Sayak"))
+console.log(Array.from("Sayak"))
+console.log(Array.from({name : "sayak"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
