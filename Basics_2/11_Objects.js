6a804bab909da35937041d863cdbf4086f9a401d
SAYAK GHOSH <56470816+Cybersayak@users.noreply.github.com>
2024-10-01 13:43:21 +0000

    Objects Part 1 completed

--- Basics_2/11_Objects.js
+++ Basics_2/11_Objects.js
@@ -1,4 +1,4 @@
-
+const mySym = Symbol("key1")
 
 const MyUser = {
     name : "Sayak",
@@ -6,14 +6,41 @@ const MyUser = {
     location : "Chandigarh",
     email : "sayak@mail.com",
     isLogged : false ,
-    LastLoginDays : ["Wednesday", "Sunday"]
+    LastLoginDays : ["Wednesday", "Sunday"],
+    [mySym] : "mykey1",
 }
 console.log(MyUser.email);
 
 console.log(MyUser["email"]);
+console.log(MyUser[mySym]);
 
 MyUser.location = "Haldia"
+
+
+// Function 
+MyUser.greeting = function() {
+    console.log("Hello Mf");
+    
+}
+console.log(MyUser.greeting); // [Function (anonymous)]
+
+
+MyUser.greet = function() {
+    console.log("Hello BC");
+    
+}
+console.log(MyUser.greet()); // Value & undefined 
+
+MyUser.greetingTwo = function() {
+    console.log(`Hello Your Daddy is Back,${this.name}`);
+    
+}
+console.log(MyUser.greetingTwo()); 
+
 
 Object.freeze(MyUser) // freeze the values in objects 
 
 console.log(MyUser);
+
+
+
