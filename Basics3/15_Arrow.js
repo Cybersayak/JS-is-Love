293103c5c299ee9a326a9e584ca16c129259c1f6
Sayak5789 <56470816+Sayak5789@users.noreply.github.com>
2025-01-19 00:18:23 +0530

    This and Arrow Functions in JavaScript

--- Basics3/15_Arrow.js
+++ Basics3/15_Arrow.js
@@ -11,7 +11,49 @@ const user = {
 // it is used in object methods, where this refers to the object that the method is attached to,
 // thus allowing the same method to be reused on different objects.
 
-user.welcomeMessage()
-user.username="sagar"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username="sagar"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// Ex 1
+// function chai() {
+//     console.log(this);
+// }
+// chai()
+
+// const chai = function () {
+//     let username= "sayaaak"
+//     console.log(this.username);
+// }
+// chai()  
+
+
+// const chai = () => {
+//     let username= "sayaaak"
+//     console.log(this.username);
+// }
+// chai()  
+
+// ARROW FUNCTIONS 
+
+// Explicit 
+const add = (num1 ,num2) => {
+    return num1 + num2
+}
+
+console.log(add(3,4));
+
+//Implicit Return 
+
+//const minus= (num1 ,num2) => num1 - num2
+const minus= (num1 ,num2) => (num1 - num2)
+
+console.log(minus(4,3));
+
+
+const myName = () => ({username : "sayaak"})
+console.log(myName());
+
 
